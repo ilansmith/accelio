5adf1c223f8fe85acfb6bdfc4577306d3886b803
Alon Grinshpoon <alongr@mellanox.com>
2016-04-19 10:32:08 +0300

    accelio/usr/rdma: Enable beacon when rdma disconnecting when connected.
    Signed-off-by: Alon Grinshpoon <alongr@mellanox.com>

--- src/usr/transport/rdma/xio_rdma_management.c
+++ src/usr/transport/rdma/xio_rdma_management.c
@@ -2705,7 +2705,7 @@ static void xio_rdma_close(struct xio_transport_base *transport)
 			  rdma_hndl);
 
 		rdma_hndl->state = XIO_TRANSPORT_STATE_CLOSED;
-		retval = xio_rdma_disconnect(rdma_hndl, 0);
+		retval = xio_rdma_disconnect(rdma_hndl, 1);
 		if (retval)
 			DEBUG_LOG("handle:%p rdma_disconnect failed, " \
 					"%m\n", rdma_hndl);
